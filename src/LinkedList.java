8d47c8d036d8ab28691f87dce63290c62a59fa59
Avitansh <aks018@ucsd.edu>
2018-07-15 17:12:24 -0700

    LinkedList data structure

---
+++ src/LinkedList.java
@@ -0,0 +1,18 @@
+class LinkedListNode{
+    int data;
+    LinkedListNode next;
+
+    public LinkedListNode(int val)
+    {
+        this.data = val;
+    }
+}
+public class LinkedList {
+    LinkedListNode head;
+
+    public LinkedList()
+    {
+        head = null;
+    }
+
+}
