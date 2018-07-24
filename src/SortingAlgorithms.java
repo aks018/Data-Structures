04fcb27b056c056fdd1762500edf70322833915c
Avitansh <aks018@ucsd.edu>
2018-07-23 18:21:11 -0700

    Merge Sort implementation

--- src/SortingAlgorithms.java
+++ src/SortingAlgorithms.java
@@ -54,12 +54,82 @@ public class SortingAlgorithms {
         array[j] = temp;
     }
 
+
+    /*
+    1) Divide the unsorted array into n partitions, each partition contains 1 element. Here the one element is considered as sorted.
+    2) Repeatedly merge partitioned units to produce new sublists until there is only 1 sublist remaining. This will be the sorted list at the end.
+     */
+
+    int[] tempMergeArr;
+
+    public void sort(int inputArr[]){
+        this.array = inputArr;
+        this.length = inputArr.length;
+        this.tempMergeArr = new int[length];
+        doMergeSort(0,length-1);
+    }
+
+    public void doMergeSort(int left, int right)
+    {
+        if(left<right)
+        {
+            int middle = left + (right-left)/2;
+
+            doMergeSort(left, middle);
+            doMergeSort(middle+1, right);
+            mergeParts(left,middle,right);
+        }
+    }
+
+    public void mergeParts(int left, int middle, int right)
+    {
+        for(int i=left; i<=right; i++)
+        {
+            tempMergeArr[i] = array[i];
+        }
+
+        int i= left;
+        int j = middle+1;
+        int k = left;
+
+        while(i<=middle && j<=right)
+        {
+            if(tempMergeArr[i] <= tempMergeArr[j])
+            {
+                array[k] = tempMergeArr[i];
+                i++;
+            }
+            else{
+                array[k] = tempMergeArr[j];
+                j++;
+            }
+            k++;
+        }
+
+        while(i<=middle)
+        {
+            array[k] = tempMergeArr[i];
+            k++;
+            i++;
+        }
+    }
+
     public static void main(String a[]){
 
         SortingAlgorithms sorter = new SortingAlgorithms();
         int[] input = {24,2,45,20,56,75,2,56,99,53,12};
         sorter.quickSort(input);
         for(int i:input){
+            System.out.print(i);
+            System.out.print(" ");
+        }
+
+        System.out.println();
+
+        int[] inputArr = {45,23,11,89,77,98,4,28,65,43};
+        SortingAlgorithms mms = new SortingAlgorithms();
+        mms.sort(inputArr);
+        for(int i:inputArr){
             System.out.print(i);
             System.out.print(" ");
         }
