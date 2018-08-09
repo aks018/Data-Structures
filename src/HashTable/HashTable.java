edd3c9a865e80bd343db74b66f9a4c23cd4ae107
Avitansh <aks018@ucsd.edu>
2018-08-09 15:33:51 -0700

    HashTable Leetcode 500.

--- src/HashTable/HashTable.java
+++ src/HashTable/HashTable.java
@@ -1,11 +1,16 @@
 package HashTable;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class HashTable {
 
     public int lengthOfLongestSubstring(String s) {
         int max = 0;
+
+        if (s.length() == 0) return 0;
 
         HashMap<Character, Integer> set = new HashMap<Character, Integer>();
 
@@ -18,5 +23,36 @@ public class HashTable {
         }
         return max;
     }
+
+    //500. Keyboard Row
+
+    public String[] findWords(String[] words) {
+        String[] strs = {"QWERTYUIOP", "ASDFGHJKL", "ZXCVBNM"};
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < strs.length; i++) {
+            for (char c : strs[i].toCharArray()) {
+                map.put(c, i);
+            }
+        }
+        List<String> answer = new ArrayList<String>();
+
+        for(String s : words)
+        {
+            int index = map.get(s.toUpperCase().charAt(0));
+            for(char c: s.toUpperCase().toCharArray())
+            {
+                if(map.get(c)!=index)
+                {
+                    index = -1;
+                    break;
+                }
+            }
+            if(index!=-1)
+                answer.add(s);
+        }
+
+        return answer.toArray(new String[0]);
+    }
+
 
 }
