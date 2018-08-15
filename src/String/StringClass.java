f8e93e721e10bb9182c7df054f945dad8356a70b
Avitansh <aks018@ucsd.edu>
2018-08-15 12:26:06 -0700

    String algorithms new algorithm uploaded

--- src/String/StringClass.java
+++ src/String/StringClass.java
@@ -1,7 +1,9 @@
 package String;
 
 import java.lang.String;
+import java.util.Arrays;
 import java.util.HashSet;
+import java.util.Set;
 
 public class StringClass {
 
@@ -188,6 +190,94 @@ public class StringClass {
         if (row == 0 && col == 0)
             return true;
         return false;
+
+    }
+
+    /*
+    Given a string, you need to reverse the order of characters in each word within a sentence while still preserving whitespace and initial word order.
+    Input: "Let's take LeetCode contest"
+    Output: "s'teL ekat edoCteeL tsetnoc"
+     */
+
+    public String reverseWords(String s) {
+        String[] strArr = s.split(" ");
+        StringBuilder sb = new StringBuilder();
+        for (String str : strArr) {
+            sb.append(reverseString(str) + " ");
+        }
+
+        return sb.toString().trim();
+
+    }
+
+    private String reverseString(String s) {
+        char[] cArray = s.toCharArray();
+        int start = 0;
+        int end = cArray.length - 1;
+
+        while (start <= end) {
+            char temp = cArray[start];
+            cArray[start] = cArray[end];
+            cArray[end] = temp;
+
+            start++;
+            end--;
+        }
+
+        return new String(cArray);
+    }
+
+    /*
+    A sentence S is given, composed of words separated by spaces. Each word consists of lowercase and uppercase letters only.
+
+    We would like to convert the sentence to "Goat Latin" (a made-up language similar to Pig Latin.)
+
+    The rules of Goat Latin are as follows:
+
+    If a word begins with a vowel (a, e, i, o, or u), append "ma" to the end of the word.
+    For example, the word 'apple' becomes 'applema'.
+
+    If a word begins with a consonant (i.e. not a vowel), remove the first letter and append it to the end, then add "ma".
+    For example, the word "goat" becomes "oatgma".
+
+    Add one letter 'a' to the end of each word per its word index in the sentence, starting with 1.
+    For example, the first word gets "a" added to the end, the second word gets "aa" added to the end and so on.
+    Return the final sentence representing the conversion from S to Goat Latin.
+
+    Input: "I speak Goat Latin"
+    Output: "Imaa peaksmaaa oatGmaaaa atinLmaaaaa"
+     */
+
+    Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U'));
+
+    public String toGoatLatin(String S) {
+        String[] strArray = S.split(" ");
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < strArray.length; i++) {
+            sb.append(conversion(strArray[i], i + 1) + " ");
+        }
+        return sb.toString().trim();
+
+    }
+
+    private String conversion(String s, int index) {
+        char[] charArr = s.toCharArray();
+        StringBuilder sb = new StringBuilder(s);
+        if (vowels.contains(charArr[0])) {
+            sb.append("ma");
+        } else {
+            char first = charArr[0];
+            sb.deleteCharAt(0);
+            sb.append(String.valueOf(first));
+            sb.append("ma");
+        }
+
+        for (int i = 0; i < index; i++) {
+            sb.append("a");
+        }
+
+        return sb.toString();
 
     }
 
